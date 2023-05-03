e9d91c5f6862f669a3bc09d65fc302c7dccc1d00
mohammademon02 <mohammademon0207@gmail.com>
2023-05-03 12:14:53 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBJyX450Q8VIcdi-_NnMEEKvGyqra18-uk",
+    authDomain: "the-eater.firebaseapp.com",
+    projectId: "the-eater",
+    storageBucket: "the-eater.appspot.com",
+    messagingSenderId: "372969687265",
+    appId: "1:372969687265:web:064ba72930285bfba24fa0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app()
