e275bc6180875c4c833fe9283a665fe7470bf2b4
mohammademon02 <mohammademon0207@gmail.com>
2023-05-04 10:49:47 +0600

    login register authentication completed

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,12 +1,56 @@
-import React, { Children, createContext } from 'react';
+import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from '../firebase/firebase.config';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-export const AuthContext = createContext(null)
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
 
 
-const AuthProvider = ({ Children }) => {
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            console.log('logged in user inside auth state observer', loggedUser)
+            setUser(loggedUser);
+            setLoading(false);
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
+    }
+
     return (
-        <AuthContext.Provider>
-            {Children}
+        <AuthContext.Provider value={authInfo}>
+            {children}
         </AuthContext.Provider>
     );
 };
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then()
+            .catch(error => console.log(error));
+    }
+
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
@@ -11,15 +22,20 @@ const Header = () => {
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mx-auto">
                         <Link to="/">Home</Link>
-                        <Nav.Link to="/blog">Blog</Nav.Link>
+                        <Link to="/blog">Blog</Link>
                         <Link to="/register">Register</Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="">profile</Nav.Link>
-                        <Button variant="secondary">Logout</Button>
-                        <Link to="/login">
-                            <Button variant="secondary">Login</Button>
-                        </Link>
+                        {
+                            user && <FaUserCircle style={{ fontSize: '40px' }}></FaUserCircle>
+                        }
+
+                        {user ?
+                            <Link><Button onClick={handleLogOut} variant="secondary">Logout</Button></Link> :
+                            <Link to="/login">
+                                <Button variant="secondary">Login</Button>
+                            </Link>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
