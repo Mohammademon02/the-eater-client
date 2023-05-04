9537d8a6baad37c8c68bf4f92bc797009736f4ba
mohammademon02 <mohammademon0207@gmail.com>
2023-05-04 16:01:57 +0600

    blog section completed

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../shared/Header/Header';
 import Footer from '../shared/Footer/Footer';
-import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 const Main = () => {
